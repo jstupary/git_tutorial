b7c15e2e169c5d38a0184bf9d4fdb65eeaf3252b
jstupary <39532902+jstupary@users.noreply.github.com>
2018-05-22 11:43:51 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+	cout << "My name is Jesse, I don't really know what's going on but that's okay :D" << endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
